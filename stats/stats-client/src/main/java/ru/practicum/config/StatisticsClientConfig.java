7be2a8f9e3aba1dd41f4f39b8475210ece4c5d10
DenisovKN <denisov.kn@yandex.ru>
2025-09-03 09:11:12 +0300

    feat: Исправить запрос клиента stats-server

--- stats/stats-client/src/main/java/ru/practicum/config/StatisticsClientConfig.java
+++ stats/stats-client/src/main/java/ru/practicum/config/StatisticsClientConfig.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.backoff.FixedBackOffPolicy;
 import org.springframework.retry.policy.MaxAttemptsRetryPolicy;
 import org.springframework.retry.support.RetryTemplate;
+import org.springframework.web.client.RestTemplate;
 import ru.practicum.StatsClient;
 
 import java.net.URI;
@@ -16,37 +18,14 @@ import java.net.URI;
 @Configuration
 public class StatisticsClientConfig {
 
-
-    @Value("${stats-server.service-id:stats-server}") // id сервиса в Eureka
-    private String statsServiceId;
-
-
     @Bean
-    public RetryTemplate retryTemplate() {
-        RetryTemplate retryTemplate = new RetryTemplate();
-
-        FixedBackOffPolicy backOffPolicy = new FixedBackOffPolicy();
-        backOffPolicy.setBackOffPeriod(3000L);
-        retryTemplate.setBackOffPolicy(backOffPolicy);
-
-        MaxAttemptsRetryPolicy retryPolicy = new MaxAttemptsRetryPolicy();
-        retryPolicy.setMaxAttempts(3);
-        retryTemplate.setRetryPolicy(retryPolicy);
-
-        return retryTemplate;
-    }
-    @Bean
-    public StatsClient statsClient(DiscoveryClient discoveryClient,
-                                   RetryTemplate retryTemplate) {
-        RestTemplateBuilder builder = new RestTemplateBuilder();
-        return new StatsClient(builder.build(),  resolveStatsServerUrl(discoveryClient, retryTemplate));
+    @LoadBalanced
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 
-    private String resolveStatsServerUrl(DiscoveryClient discoveryClient, RetryTemplate retryTemplate) {
-        ServiceInstance instance = retryTemplate.execute(ctx ->
-                discoveryClient.getInstances(statsServiceId).getFirst()
-        );
-        URI uri = URI.create("http://" + instance.getHost() + ":" + instance.getPort());
-        return uri.toString();
+    @Bean
+    public StatsClient statsClient(RestTemplate restTemplate) {
+        return new StatsClient(restTemplate, "http://stats-server");
     }
 }
