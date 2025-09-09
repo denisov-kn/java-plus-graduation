dd9d037127c9561598136e736bf3217a813de16a
DenisovKN <denisov.kn@yandex.ru>
2025-09-09 07:36:44 +0300

    feat: Поправить название сервиса events на event-service, requests на request-service

--- core/comments/src/main/java/ru/practicum/clients/EventClient.java
+++ core/comments/src/main/java/ru/practicum/clients/EventClient.java
@@ -3,6 +3,6 @@ package ru.practicum.clients;
 import org.springframework.cloud.openfeign.FeignClient;
 import ru.practicum.api.events.EventInternalApi;
 
-@FeignClient(name = "events")
+@FeignClient(name = "event-service")
 public interface EventClient extends EventInternalApi {
 }
